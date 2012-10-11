9e4bc366baebf1b38997112b7f6202ef502d43e6
kurron <kurr@kurron.org>
2012-10-11 13:31:50 -0400

    Figured out how to put in text.

--- src/main/groovy/org/kurron/pdf/PDFBuilder.groovy
+++ src/main/groovy/org/kurron/pdf/PDFBuilder.groovy
@@ -2,18 +2,37 @@ package org.kurron.pdf
 
 import org.pdfbox.pdmodel.PDDocument
 import org.pdfbox.pdmodel.PDPage
+import org.pdfbox.pdmodel.font.PDType1Font
+import org.pdfbox.pdmodel.font.PDFont
+import org.pdfbox.pdmodel.edit.PDPageContentStream
 
 /**
  * Assembles the bits and pieces of a PDF together into a single document.
  */
 class PDFBuilder 
 {
-    def build()
+    def addPage()
+    {
+    }
+
+    def byte[] build()
     {
         PDDocument document = new PDDocument()
-        PDPage blankPage = new PDPage()
-        document.addPage(blankPage)
-        document.save( "blank.pdf")
+        PDPage page = new PDPage()
+
+        PDFont font = PDType1Font.HELVETICA_BOLD
+        PDPageContentStream contentStream = new PDPageContentStream(document, page)
+        contentStream.beginText()
+        contentStream.setFont( font, 12 )
+        contentStream.moveTextPositionByAmount( 100, 700 )
+        contentStream.drawString( "Hello World" )
+        contentStream.endText()
+        contentStream.close()
+
+        document.addPage(page)
+        final stream = new ByteArrayOutputStream(1024)
+        document.save(stream)
         document.close()
+        stream.toByteArray()
     }
 }
--- src/test/groovy/org/kurron/pdf/PDFBuilderUnitTest.groovy
+++ src/test/groovy/org/kurron/pdf/PDFBuilderUnitTest.groovy
@@ -7,14 +7,43 @@ import spock.lang.Specification
  */
 class PDFBuilderUnitTest extends Specification
 {
-    def "given_when_then"() {
-        given: "concrete builder"
-        def builder = new PDFBuilder()
+    def sut = new PDFBuilder()
+
+    def "given_no_added_text_when_build_is_called_then_blank_page_is_generated"() {
+        given: "no added text"
 
         when: "build is called"
-        builder.build()
+        final bytes = sut.build()
 
         then:  "pdf file is created"
+        assert bytes != null
+        assert bytes.length > 0
+
+        saveToFile(bytes, "blank.pdf")
+
         println 'done!'
+    }
+
+    def "given_added_text_when_build_is_called_then_page_contains_text"() {
+        given: "no added text"
+
+        when: "build is called"
+        final bytes = sut.build()
+
+        then:  "pdf file is created"
+        assert bytes != null
+        assert bytes.length > 0
+
+        saveToFile(bytes, "text.pdf")
+
+        println 'done!'
+    }
+
+    def void saveToFile(byte[] bytes, final String name) {
+        File file = new File(name)
+        final stream = file.newOutputStream()
+        stream.write(bytes)
+        stream.flush()
+        stream.close()
     }
 }
