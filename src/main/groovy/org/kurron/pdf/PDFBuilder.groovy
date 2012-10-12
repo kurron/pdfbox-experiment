a887235ed19614c2b5e1861c34fe3afe288acc98
kurron <kurr@kurron.org>
2012-10-12 14:45:19 -0400

    Updated to current version of PDFBox and added a chinese character.

--- src/main/groovy/org/kurron/pdf/PDFBuilder.groovy
+++ src/main/groovy/org/kurron/pdf/PDFBuilder.groovy
@@ -1,12 +1,11 @@
 package org.kurron.pdf
 
-import org.pdfbox.pdmodel.PDDocument
-import org.pdfbox.pdmodel.PDPage
-import org.pdfbox.pdmodel.font.PDType1Font
-import org.pdfbox.pdmodel.font.PDFont
-import org.pdfbox.pdmodel.edit.PDPageContentStream
-import org.codehaus.groovy.reflection.ReflectionUtils
-import org.pdfbox.pdmodel.graphics.xobject.PDJpeg
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream
+import org.apache.pdfbox.pdmodel.font.PDFont
+import org.apache.pdfbox.pdmodel.font.PDTrueTypeFont
+import org.apache.pdfbox.pdmodel.graphics.xobject.PDJpeg
 
 /**
  * Assembles the bits and pieces of a PDF together into a single document.
@@ -28,20 +27,20 @@ class PDFBuilder
 
         PDPage page = new PDPage()
 
-        PDFont font = PDType1Font.HELVETICA_BOLD
+        //PDFont font = PDType1Font.HELVETICA_BOLD
+        InputStream fontStream = PDFBuilder.class.getResourceAsStream("/HeiT.ttf")
+        assert fontStream != null
+        PDFont font = PDTrueTypeFont.loadTTF(document, fontStream)
         PDPageContentStream contentStream = new PDPageContentStream(document, page)
-        contentStream.drawImage( jpeg, 0, 200, 800, 600 );
+        contentStream.drawImage( jpeg, 0, 0 );
 
         contentStream.beginText()
-        contentStream.setFont( font, 48 )
+        contentStream.setFont( font, 72 )
         contentStream.moveTextPositionByAmount( 0, 600 )
-        contentStream.drawString( "Hello World" )
+        contentStream.drawString( new StringBuilder().append( '\u4e1e').toString() )
         contentStream.endText()
 
-
         contentStream.close()
-
-
 
         document.addPage(page)
         final stream = new ByteArrayOutputStream(1024)
